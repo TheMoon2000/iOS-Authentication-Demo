91954ca1d6c8a55b798f992668af630706057eb9
TheMoon2000 <jiarui.shan@berkeley.edu>
2019-02-18 13:43:41 -0800

    Initial Commit

---
+++ Authentication Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Authentication Demo
+//
+//  Created by Jia Rui Shan on 2019/2/18.
+//  Copyright © 2019 UC Berkeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
